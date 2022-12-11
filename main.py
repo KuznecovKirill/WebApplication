50159c3cce2470cae20ee887ba488a3b96c7fd5b
Kirill <kirillkuznecov03@mail.ru>
2022-12-11 13:21:40 +0500

    Таблица для базы данных

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///Магазин.db'
-db = SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Магазин.db'
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True) #id товара
+    name = db.Column(db.String(50), nullable=False) # название
+    price = db.Column(db.Integer, nullable=False) #цена
+    #picture = db.Column(db.Text, unique=True, nullable=False) #картинка (возможно)
+
+
 @app.route('/')
 def mainMenu(): #функция главной страницы
     return render_template("mainMenu.html")
